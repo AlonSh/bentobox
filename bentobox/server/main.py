a1646ecc28722dfe7d26aa5f2d5e89ee2e8d8922
Alon Shenkler <alonshenk@gmail.com>
2018-08-26 19:31:24 +0300

    Almost working with pytun, two headers missing for some fucking reason

--- bentobox/server/main.py
+++ bentobox/server/main.py
@@ -1,30 +1,49 @@
 import socket
 import fcntl
 import struct
+import pytun
 
 from scapy.all import *
-from scapy.layers.inet import IP, ICMP
-
+from scapy.layers.inet import IP, ICMP, Ether
 
 CLIENT_INNER_IP_TO_INTERNET_IP = {}
+GATEWAY_MAC = '06:03:61:31:25:aa'
+
+tunnel_socket = pytun.TunTapDevice("tun0")
+# tunnel_socket.addr = '10.0.1.1'
+# tunnel_socket.dstaddr = '0.0.0.0'
+# tunnel_socket.netmask = '255.255.255.0'
+# tunnel_socket.mtu = 1472
+
+print(os.system('ifconfig'))
 
 
 def packet_callback(p: Packet):
-    if 'ICMP' in p and p['ICMP'].chksum == 1337 and p.sniffed_on == 'eth0':
+    if 'ICMP' in p and p.sniffed_on == 'eth0':
         print("Forwarding TCP message!")
         inner_ip_packet = IP(p['ICMP'].payload.load)
+        # ls(inner_ip_packet)
         if 'TCP' not in inner_ip_packet:
+            print('inner is not TCP')
             return
 
         CLIENT_INNER_IP_TO_INTERNET_IP[inner_ip_packet.src] = p['IP'].src
 
-        del inner_ip_packet.chksum
-        del inner_ip_packet['TCP'].chksum
-        send(inner_ip_packet, iface='tun0')
+        print(bytes(inner_ip_packet))
+        # wrapped_ether = Ether() / inner_ip_packet
+        tunnel_socket.write(bytes(inner_ip_packet))
+
+        # ls(wrapped_ether)
+        # sendp(wrapped_ether, iface='tun0')
 
     if 'TCP' in p and p.sniffed_on == 'tun0':
+        packet_destination = p['IP'].dst
+        if packet_destination not in CLIENT_INNER_IP_TO_INTERNET_IP:
+            print('Ignoring outgoing packet')
+            return
         print('Sending back the answer')
-        icmp_packet = IP(dst=CLIENT_INNER_IP_TO_INTERNET_IP[p['IP'].dst]) / ICMP(chksum=1337, type=0) / p
+        wrapped_icmp_destination = CLIENT_INNER_IP_TO_INTERNET_IP[packet_destination]
+        icmp_packet = IP(dst=wrapped_icmp_destination) / ICMP(chksum=1337, type=0) / p
         ls(icmp_packet)
         send(icmp_packet)
 
